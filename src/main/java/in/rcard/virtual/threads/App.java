9063db00c2635f9b607dd18cfb06aed75ff422e9
rcardin <riccardo.cardin@gmail.com>
2023-01-19 09:14:34 +0100

    Added logback module and start creating a logging utility

--- src/main/java/in/rcard/virtual/threads/App.java
+++ src/main/java/in/rcard/virtual/threads/App.java
@@ -3,32 +3,42 @@ package in.rcard.virtual.threads;
 import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
-  public static void main(String[] args) {
-    stackOverFlowErrorExample();
+
+  static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) throws Exception {
+    createNewVirtualThreadWithFactory();
   }
 
   private static void stackOverFlowErrorExample() {
     for (int i = 0; i < 100_000; i++) {
-      new Thread(() -> {
-        try {
-          Thread.sleep(Duration.ofSeconds(1L));
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
-        }
-      }).start();
+      new Thread(
+              () -> {
+                try {
+                  Thread.sleep(Duration.ofSeconds(1L));
+                } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+                }
+              })
+          .start();
     }
   }
-  
-  private static Thread createNewVirtualThreadWithFactory() {
-    return Thread.ofVirtual().start(() -> System.out.println("Hello from a virtual thread!"));
+
+  private static void createNewVirtualThreadWithFactory() throws InterruptedException {
+    final Thread virtualThread = Thread.ofVirtual()
+                             .start(() -> logger.info("Hello from a virtual thread!"));
+    virtualThread.setName("pippo");
+    virtualThread.join(1000L);
   }
-  
+
   private static void createNewVirtualThreadWithExecutorService()
       throws ExecutionException, InterruptedException {
     try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-      var future = executorService.submit(() -> System.out.println("Hello from a virtual thread!"));
+      var future = executorService.submit(() -> logger.info("Hello from a virtual thread!"));
       future.get();
     }
   }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module virtual.threads.playground {
   requires jdk.incubator.concurrent;
+  requires org.slf4j;
 }
