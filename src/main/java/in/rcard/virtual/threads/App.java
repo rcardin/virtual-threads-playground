5928754186f8e97c7c1d680228262c62fecb2696
rcardin <riccardo.cardin@gmail.com>
2023-01-20 15:30:27 +0100

    Added an example of two virtual threads interleaving

--- src/main/java/in/rcard/virtual/threads/App.java
+++ src/main/java/in/rcard/virtual/threads/App.java
@@ -1,8 +1,7 @@
 package in.rcard.virtual.threads;
 
 import java.time.Duration;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,36 +9,49 @@ public class App {
 
   static final Logger logger = LoggerFactory.getLogger(App.class);
 
-  public static void main(String[] args) throws Exception {
-    createNewVirtualThreadWithFactory();
+  public static void main(String[] args) {
+    concurrentMorningRoutine();
   }
 
   private static void stackOverFlowErrorExample() {
     for (int i = 0; i < 100_000; i++) {
-      new Thread(
-              () -> {
-                try {
-                  Thread.sleep(Duration.ofSeconds(1L));
-                } catch (InterruptedException e) {
-                  throw new RuntimeException(e);
-                }
-              })
+      new Thread(() -> sleep(Duration.ofSeconds(1L)))
           .start();
     }
   }
-
-  private static void createNewVirtualThreadWithFactory() throws InterruptedException {
-    final Thread virtualThread = Thread.ofVirtual()
-                             .start(() -> logger.info("Hello from a virtual thread!"));
-    virtualThread.setName("pippo");
-    virtualThread.join(1000L);
+  
+  @SneakyThrows
+  static void concurrentMorningRoutine() {
+    var vt1 = bathTime();
+    var vt2 = boilingWater();
+    vt1.join();
+    vt2.join();
   }
-
-  private static void createNewVirtualThreadWithExecutorService()
-      throws ExecutionException, InterruptedException {
-    try (var executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-      var future = executorService.submit(() -> logger.info("Hello from a virtual thread!"));
-      future.get();
-    }
+  
+  static Thread bathTime() {
+    return virtualThread("Bath time", () -> {
+      logger.info("I'm going to take a bath");
+      sleep(Duration.ofMillis(500L));
+      logger.info("I'm done with the bath");
+    });
+  }
+  
+  static Thread boilingWater() {
+    return virtualThread("Boil some water", () -> {
+      logger.info("I'm going to boil some water");
+      sleep(Duration.ofSeconds(1L));
+      logger.info("I'm done with the water");
+    });
+  }
+  
+  @SneakyThrows
+  private static void sleep(Duration duration) {
+    Thread.sleep(duration);
+  }
+  
+  private static Thread virtualThread(String name, Runnable runnable) {
+    return Thread.ofVirtual()
+                 .name(name)
+                 .start(runnable);
   }
 }
