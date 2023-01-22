916f71c27d0bf43a1aae467c04e650e0abb40b70
rcardin <riccardo.cardin@gmail.com>
2023-01-22 21:26:49 +0100

    Added the concurrentMorningRoutineUsingExecutors function

--- src/main/java/in/rcard/virtual/threads/App.java
+++ src/main/java/in/rcard/virtual/threads/App.java
@@ -1,6 +1,7 @@
 package in.rcard.virtual.threads;
 
 import java.time.Duration;
+import java.util.concurrent.Executors;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +11,15 @@ public class App {
   static final Logger logger = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
-    concurrentMorningRoutine();
+    concurrentMorningRoutineUsingExecutors();
   }
 
   private static void stackOverFlowErrorExample() {
     for (int i = 0; i < 100_000; i++) {
-      new Thread(() -> sleep(Duration.ofSeconds(1L)))
-          .start();
+      new Thread(() -> sleep(Duration.ofSeconds(1L))).start();
     }
   }
-  
+
   @SneakyThrows
   static void concurrentMorningRoutine() {
     var vt1 = bathTime();
@@ -27,31 +27,55 @@ public class App {
     vt1.join();
     vt2.join();
   }
-  
+
+  @SneakyThrows
+  static void concurrentMorningRoutineUsingExecutors() {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      var f1 =
+          executor.submit(
+              () -> {
+                logger.info("I'm going to take a bath");
+                sleep(Duration.ofMillis(500L));
+                logger.info("I'm done with the bath");
+              });
+      var f2 =
+          executor.submit(
+              () -> {
+                logger.info("I'm going to boil some water");
+                sleep(Duration.ofSeconds(1L));
+                logger.info("I'm done with the water");
+              });
+      f1.get();
+      f2.get();
+    }
+  }
+
   static Thread bathTime() {
-    return virtualThread("Bath time", () -> {
-      logger.info("I'm going to take a bath");
-      sleep(Duration.ofMillis(500L));
-      logger.info("I'm done with the bath");
-    });
+    return virtualThread(
+        "Bath time",
+        () -> {
+          logger.info("I'm going to take a bath");
+          sleep(Duration.ofMillis(500L));
+          logger.info("I'm done with the bath");
+        });
   }
-  
+
   static Thread boilingWater() {
-    return virtualThread("Boil some water", () -> {
-      logger.info("I'm going to boil some water");
-      sleep(Duration.ofSeconds(1L));
-      logger.info("I'm done with the water");
-    });
+    return virtualThread(
+        "Boil some water",
+        () -> {
+          logger.info("I'm going to boil some water");
+          sleep(Duration.ofSeconds(1L));
+          logger.info("I'm done with the water");
+        });
   }
-  
+
   @SneakyThrows
   private static void sleep(Duration duration) {
     Thread.sleep(duration);
   }
-  
+
   private static Thread virtualThread(String name, Runnable runnable) {
-    return Thread.ofVirtual()
-                 .name(name)
-                 .start(runnable);
+    return Thread.ofVirtual().name(name).start(runnable);
   }
 }
